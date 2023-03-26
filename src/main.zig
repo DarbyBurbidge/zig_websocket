beb6054a7602702ff679a1d5ce78746a319ce7f4
DarbyBurbidge <darby.burbidge@gmail.com>
2023-03-25 23:55:28 -0700

    server can connect

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,25 @@
 const std = @import("std");
 const http = @import("http.zig");
 const WebSocket = @import("socket.zig");
+pub const io_mode = .evented;
 
-pub fn main() void {
-    http.shout();
+const net = std.net;
+const StreamServer = net.StreamServer;
+const Address = net.Address;
+
+pub fn main() anyerror!void {
+    var server = StreamServer.init(.{});
+    defer server.close();
+    const address = try Address.resolveIp("127.0.0.1", 8080);
+    try server.listen(address);
+
+    while(true) {
+        const connection = try server.accept();
+        try handler(connection.stream);
+    }
+}
+
+fn handler(stream: net.Stream) !void {
+    defer stream.close();
+    try stream.writer().print("Hello World", .{});
 }
